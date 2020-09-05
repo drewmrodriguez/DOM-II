5a3743215bf527c1de9c245fbd66dabc0dac5645
drewmrodriguez <drewmrodriguez@gmail.com>
2020-09-05 11:07:16 -0700

    mouseover, dbclick, &keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-// Your code goes here
+// 1. Mouseover
+let logoHeading = document.querySelector('.logo-heading')
+logoHeading.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'orange'
+});
+
+// 2. dbclick
+const logoHeader = document. querySelector('.main-navigation');
+logoHeader.addEventListener('dblclick', function() {
+    logoHeader.style.backgroundColor = 'skyblue';
+});
+
+// 3. keydown
+const bodyColor = document.querySelector('body');
+bodyColor.addEventListener('keydown', e => {
+    e.target.style.backgroundColor =  "lightgrey";
+});
+
+// 4.
+
+
+
+// 5.
+
+
+
+// 6.
+
+
+
+// 7.
+
+
+
+// 8.
+
+
+
+// 9.
+
+
+
+// 10. 
