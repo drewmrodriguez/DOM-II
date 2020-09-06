ecc8d050ec13c72b67c7b5f2d6efba66a0d0c2ce
drewmrodriguez <drewmrodriguez@gmail.com>
2020-09-05 19:05:20 -0700

    mouseenter. mouseleave, and resize

--- js/index.js
+++ js/index.js
@@ -16,16 +16,23 @@ bodyColor.addEventListener('keydown', e => {
     e.target.style.backgroundColor =  "lightgrey";
 });
 
-// 4.
+// 4. mouseenter
+const imgHover = document.querySelector("img")
+imgHover.addEventListener("mouseenter", () => {
+  imgHover.style.transform ="scale(1.2)";
+  imgHover.style.transition = "all 0.3s"
+});
 
-
-
-// 5.
-
-
+// 5. mouseleave
+imgHover.addEventListener("mouseleave", () => {
+    imgHover.style.transform ="scale(1)";
+  });
 
 // 6.
-
+const img_1 = document.querySelector("img")
+window.addEventListener('resize', () => {
+    img_1.src = "https://images.unsplash.com/photo-1598944675794-1fe37b6bc197?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+  });
 
 
 // 7.
